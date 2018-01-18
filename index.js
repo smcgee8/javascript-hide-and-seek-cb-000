42b1d963df6bab31562da8399113079de66c5e42
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-18 21:13:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  lis = document.querySelectorAll('ul.ranked-list li');
+  var lis = document.querySelectorAll('ul.ranked-list li');
   for (let i = 0; i < lis.length; i++) {
     lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
   }
